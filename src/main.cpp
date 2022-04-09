08a2f6406faa299dbcd8eb2102f384c3cffc3126
blamster19 <blamster19@gmail.com>
2022-04-09 23:56:17 +0200

    Add main argument parsing stub
    Now the program will accept some options from the terminal. Currently the user can toggle on verbosity and extra verbosity.

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,33 @@
 #include "../deps/Plotter.h"
 #include "generator.hpp"
 #include <cstdlib>
+#include <string>
+
+enum msgCodes{MSG_invArg, MSG_noFile};
+void verbIt(char code, char* arg, char* name);
 
 int main(int argc, char **argv){
-	city argleton;
+	bool verbose = 0;
+	bool extraverbose = 0;
+	//parse args
+	int token = 1;
+	std::string argument;
+	while(argv[token] != nullptr){
+		if(argv[token][0] != '-'){
+			verbIt(MSG_invArg, argv[token], argv[0]);
+		}else{
+			argument = std::string(argv[token]);
+			if(argument == "-v" || argument == "--verbose"){
+				verbose = 1;
+			}
+			if(argument == "-V" || argument == "--Verbose"){
+				verbose = 1;
+				extraverbose = 1;
+			}
+		}
+		token++;
+	}
+	city argleton(verbose, extraverbose);
 	populateCity(argleton);
 	Plotter mapGen;
 	for(int i = 0; i < 50; i++)
@@ -13,4 +37,11 @@ int main(int argc, char **argv){
 		argleton.movePeople();
 	}
 	return 0;
+}
+
+void verbIt(char code, char* arg, char* name){
+	switch(code){
+		case MSG_invArg:
+			printf("%s: Invalid option: '%s''\nTry '%s --help' for more information.", name, arg, name);
+	}
 }
