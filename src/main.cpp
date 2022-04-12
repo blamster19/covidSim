7509aecd577f497d50b72b3b86d83a5d879d4754
blamster19 <blamster19@gmail.com>
2022-04-12 22:54:16 +0200

    Add duplicate argument checker and status messages
    Program returns 1 if the same argument is passed more than once; A few status messages like 'invlid option', 'no such file' and help page (contents not written yet) added.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <cstdlib>
 #include <string>
 
-enum msgCodes{MSG_invArg, MSG_noFile};
+enum msgCodes{MSG_invArg, MSG_noFile, MSG_dupliArg, MSG_help, MSG_conflict};
 void verbIt(char code, char* arg, char* name);
 
 int main(int argc, char **argv){
@@ -16,18 +16,36 @@ int main(int argc, char **argv){
 	while(argv[token] != nullptr){
 		if(argv[token][0] != '-'){
 			verbIt(MSG_invArg, argv[token], argv[0]);
+			return 1;
 		}else{
 			argument = std::string(argv[token]);
 			if(argument == "-v" || argument == "--verbose"){
+				if(verbose == 1){
+					verbIt(MSG_dupliArg, argv[token], argv[0]);
+					return  1;
+				}
 				verbose = 1;
 			}
 			if(argument == "-V" || argument == "--Verbose"){
+				if(verbose == 1 || extraverbose == 1){
+					verbIt(MSG_dupliArg, argv[token], argv[0]);
+					return 1;
+				}
 				verbose = 1;
 				extraverbose = 1;
+			}
+			if(argument == "-h" || argument == "--help"){
+				if(argc > 2){
+					verbIt(MSG_conflict, argv[0], argv[0]);
+					return 1;
+				}
+				verbIt(MSG_help, argv[0], argv[0]);
+				return 0;
 			}
 		}
 		token++;
 	}
+
 	city argleton(verbose, extraverbose);
 	populateCity(argleton);
 	Plotter mapGen;
@@ -42,6 +60,18 @@ int main(int argc, char **argv){
 void verbIt(char code, char* arg, char* name){
 	switch(code){
 		case MSG_invArg:
-			printf("%s: Invalid option: '%s''\nTry '%s --help' for more information.", name, arg, name);
+			printf("%s: Invalid option: '%s''\nTry '%s --help' for more information.\n", name, arg, name);
+			break;
+		case MSG_noFile:
+			printf("%s: %s: No such file or directory\n", name, arg);
+			break;
+		case MSG_dupliArg:
+			printf("%s: Supplied the same argument twice: %s\n", name, arg);
+			break;
+		case MSG_help:
+			printf("###[ help text ]###]\n");
+			break;
+		case MSG_conflict:
+			printf("%s: Conflicting arguments\nTry '%s --help' for more information.\n", name, name);
 	}
 }
