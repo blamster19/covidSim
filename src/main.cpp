blamster19/covidSim

commit 76da7982b5976a0263a4ca89aa73ba033709d496

Fix '--output' flag parser
Now '--output' accepts "true", "TRUE", "false", "FALSE", '1', '0' as options. No flag handling yet, just parsing.

src/main.cpp
@@ -181,6 +181,17 @@ bool parseParams(int argc, char **argv, bool &verbose, bool &extraverbose, char 
 				if(argv[token][0] == '-'){
 					goto notAnOpt;
 				}
+				argument = argv[token];
+				if(argument == "true" || argument == "TRUE" || argument == "1"){
+					flags += 0b00001000;
+					parsedArgs[4] = "1";
+				}else
+				if(argument == "false" || argument == "FALSE" || argument == "0"){
+					flags += 0b00001000;
+					parsedArgs[4] = "0";
+				}else{
+					goto invalidArg;
+				}
 				flags += 0b00001000;
 				parsedArgs[4] = argv[token];
 			}else
@@ -217,8 +228,7 @@ bool parseParams(int argc, char **argv, bool &verbose, bool &extraverbose, char 
 				}else if(argument == "test"){// default
 					flags += 0b00000001;
 				}else{
-					verbIt(MSG_invArg, argv[token], argv[0]);
-					return 1;
+					goto invalidArg;
 				}
 				parsedArgs[6] = argv[token];
 			}else{
@@ -234,5 +244,8 @@ bool parseParams(int argc, char **argv, bool &verbose, bool &extraverbose, char 
 		return 1;
 	notAnOpt:// no option value provided
 		verbIt(MSG_noArg, argv[token-1], argv[0]);
+		return 1;
+	invalidArg:// wrong argument value
+		verbIt(MSG_invArg, argv[token], argv[0]);
 		return 1;
 }

commit b2c9ef83f5090e4ed7f7059bc0d4403ca926ef8a

Fix '--output' parser to follow convention
True-false options should pass their status through the flags variable and not through passedArgs[] array. Commit 76da798 (Fix '--output' flag parser, 2022-04-19) mistakenly assumed the latter and passed the status the other way, but, unlike 'verbose', it sets the status based on the option _value_, and not the flag's presence only ('--verbose' sets verbosity to true just by appearing as a terminal argument, '--output' sets 'output' based on true/false that is passed after it).

src/main.cpp
@@ -184,16 +184,12 @@ bool parseParams(int argc, char **argv, bool &verbose, bool &extraverbose, char 
 				argument = argv[token];
 				if(argument == "true" || argument == "TRUE" || argument == "1"){
 					flags += 0b00001000;
-					parsedArgs[4] = "1";
 				}else
 				if(argument == "false" || argument == "FALSE" || argument == "0"){
-					flags += 0b00001000;
-					parsedArgs[4] = "0";
+					// do nothing
 				}else{
 					goto invalidArg;
 				}
-				flags += 0b00001000;
-				parsedArgs[4] = argv[token];
 			}else
 			if(argument == "--doFrames"){
 				if((flags|0b00000100) == flags){